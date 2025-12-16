1ea2b73a238df19b66591821d7ca2e1629b25c55
Steven Hepting <shepting@gmail.com>
2025-12-16 10:29:19 -0800

    Remove beads tooling and relocate fastlane Ruby setup (#24)
    * Add dark mode support
    
    - Update UIColor extension with dynamic colors for light/dark mode
    - Implement UITabBarAppearance for iOS 13+ dark mode compatibility
    - barTint adapts to dark gray in dark mode, black in light mode
    - background adapts to black in dark mode, light gray in light mode
    - tint color (orange) remains consistent across both modes
    
    * chore: remove beads and relocate fastlane ruby setup

--- FathomUIKit/Extension/UIColor+Extension.swift
+++ FathomUIKit/Extension/UIColor+Extension.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 extension UIColor {
-    public static let barTint: UIColor = UIColor.black
-    public static let tint: UIColor = #colorLiteral(red: 1, green: 0.5803921569, blue: 0.1450980392, alpha: 1)
-    public static let background: UIColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+    public static let barTint: UIColor = {
+        if #available(iOS 13.0, *) {
+            return UIColor { traitCollection in
+                switch traitCollection.userInterfaceStyle {
+                case .dark:
+                    return UIColor(red: 0.11, green: 0.11, blue: 0.12, alpha: 1.0) // Dark gray
+                default:
+                    return UIColor.black
+                }
+            }
+        } else {
+            return UIColor.black
+        }
+    }()
+
+    public static let tint: UIColor = #colorLiteral(red: 1, green: 0.5803921569, blue: 0.1450980392, alpha: 1) // Orange - same in both modes
+
+    public static let background: UIColor = {
+        if #available(iOS 13.0, *) {
+            return UIColor { traitCollection in
+                switch traitCollection.userInterfaceStyle {
+                case .dark:
+                    return UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0) // Black
+                default:
+                    return #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1) // Light gray
+                }
+            }
+        } else {
+            return #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+        }
+    }()
 }
